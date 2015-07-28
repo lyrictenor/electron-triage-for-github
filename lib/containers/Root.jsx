39152d7dcd932575cb0248ab815b60c8c568d85c
sanemat <o.gata.ken@gmail.com>
2015-07-28 22:52:02 +0900

    chore(connector): use connector

--- lib/containers/Root.jsx
+++ lib/containers/Root.jsx
@@ -1,6 +1,13 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';// eslint-disable-line no-unused-vars
+// github.com/babel/babel-eslint/issues/72
+// Using decorator doesn't count as usage by eslint
 
-export default class Root extends Component {
+@connect(state => ({
+  router: state.router,
+  setting: state.setting
+}))
+class Root extends Component {
   render () {
     return (
       <div>
