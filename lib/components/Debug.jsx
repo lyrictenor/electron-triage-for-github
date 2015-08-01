872385f4211c5b267b36633d2a396f583f2f101b
sanemat <o.gata.ken@gmail.com>
2015-08-01 21:26:25 +0900

    feat(debug): reset storage button in debug

--- lib/components/Debug.jsx
+++ lib/components/Debug.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import resetStorage from 'reset-storage';
+import { databaseName } from '../../package.json';
 
 export default class Debug extends Component {
+  handleResetStorage () {
+    resetStorage(databaseName).then(() => {
+      console.log(`Reset localStorage and indexedDB: ${databaseName}`);//eslint-disable-line no-console
+    });
+  }
   render() {
     return (
-      <div>Debug, <Link to={'/settings'}>settings</Link>, <Link to={'/home'}>home</Link></div>
+      <div>
+        Debug, <Link to={'/settings'}>settings</Link>, <Link to={'/home'}>home</Link>
+        <button onClick={this.handleResetStorage}>Reset storage</button>
+      </div>
     );
   }
 }
