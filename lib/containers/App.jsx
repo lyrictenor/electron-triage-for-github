d49aa53be79274787c3f7813349e55b3e185e4c5
sanemat <o.gata.ken@gmail.com>
2015-08-11 11:56:42 +0900

    chore(devtools): remove devtools

--- lib/containers/App.jsx
+++ lib/containers/App.jsx
@@ -3,38 +3,12 @@ import { Provider } from 'react-redux';
 import { Router, Route } from 'react-router';
 import { history } from 'react-router/lib/HashHistory';
 import { reduxRouteComponent } from 'redux-react-router';
-import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
 import Root from './Root.jsx';
 import Home from '../components/Home.jsx';
 import Settings from '../components/Settings.jsx';
 import store from '../utils/store';
 import Debug from '../components/Debug.jsx';
-
-let contents = [
-  <Provider store={store}>
-    {() =>
-      <Router history={history}>
-        <Route component={reduxRouteComponent(store)}>
-          <Route path='/' component={Root}>
-            <Route path='home' component={Home} />
-            <Route path='settings' component={Settings} />
-            <Route path='debug' component={Debug} />
-          </Route>
-        </Route>
-      </Router>
-    }
-  </Provider>
-];
-
-if (process.env.NODE_ENV === 'development') {
-  contents.push(
-    <DebugPanel top right bottom>
-      <DevTools store={store}
-                monitor={LogMonitor} />
-    </DebugPanel>
-  );
-}
 
 export default class App extends Component {
   constructor(props) {
@@ -44,9 +18,19 @@ export default class App extends Component {
   // github.com/rackt/react-router/issues/1570
   render() {
     return (
-      <div>
-        {contents}
-      </div>
+      <Provider store={store}>
+        {() =>
+          <Router history={history}>
+            <Route component={reduxRouteComponent(store)}>
+              <Route path='/' component={Root}>
+                <Route path='home' component={Home} />
+                <Route path='settings' component={Settings} />
+                <Route path='debug' component={Debug} />
+              </Route>
+            </Route>
+          </Router>
+        }
+      </Provider>
     );
   }
 }
