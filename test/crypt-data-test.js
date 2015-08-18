b637c458929f77acb397a3f25ca5aa7f7a99d71b
sanemat <o.gata.ken@gmail.com>
2015-08-18 20:42:29 +0900

    test(crypt): add test(fail)

--- test/crypt-data-test.js
+++ test/crypt-data-test.js
@@ -5,3 +5,10 @@ import { encryptData, decryptData } from '../lib/utils/cryptData';
 it('should not encrypt empty string', () => {
   assert.strictEqual(encryptData(''), '');
 });
+it('should decrypt empty string', () => {
+  assert.strictEqual(decryptData(''), '');
+});
+it('should decrypt encrypted value', () => {
+  const expected = '1234';
+  assert.strictEqual(decryptData(encryptData(expected)), expected);
+});
