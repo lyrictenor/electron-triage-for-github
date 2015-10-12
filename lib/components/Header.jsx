98ade446b73f52b0142ac7cc84391e0ae2f68011
sanemat <o.gata.ken@gmail.com>
2015-10-13 00:37:40 +0900

    chore(debug): add left nav

--- lib/components/Header.jsx
+++ lib/components/Header.jsx
@@ -1,15 +1,34 @@
 import React, { Component, PropTypes } from 'react';
 import {
   AppBar,
+  LeftNav,
 } from 'material-ui';
 
 export default class Header extends Component {
+  handleToggleNav(event) {
+    event.preventDefault();
+    this.refs.leftNav.toggle();
+  }
   render() {
     const { title } = this.props;
+    // FIXME: selectedIndex
+    const menuItems = [
+      { route: 'home', text: 'Home' },
+      { route: 'settings', text: 'Settings' },
+      { route: 'debug', text: 'Debug' },
+    ];
     return (
-      <AppBar
-        title={title}
-        />
+      <div>
+        <AppBar
+          title={title}
+          onLeftIconButtonTouchTap={this.handleToggleNav.bind(this)}
+          />
+        <LeftNav
+          ref="leftNav"
+          docked={false}
+          menuItems={menuItems}
+          />
+      </div>
     );
   }
 }
